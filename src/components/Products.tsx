65a363251b2e980b852d06c4607e7509eec535fd
gringrape <gringrape200@gmail.com>
2021-12-19 18:41:13 +0900

    Style product list

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,15 +1,50 @@
+import styled from 'styled-components';
+
 import { useProducts } from '../hooks';
+
+const ThumbnailImage = styled.img`
+  width: 210px;
+  height: 210px;
+  object-fit: cover;
+  object-position: center top;
+  border-radius: 12px;
+`;
+
+const List = styled.ul`
+  display: grid;
+  grid: auto-flow / repeat(4, 1fr);
+  gap: 1.5rem;
+
+  @media (max-width: 768px) {
+    grid: auto-flow / 1fr 1fr 1fr;
+  }
+
+  @media (max-width: 660px) {
+    grid: auto-flow / 1fr 1fr;
+  }
+`;
+
+const ListItem = styled.li`
+  list-style: none;
+  max-width: 250px;
+`;
+
+const Title = styled.p`
+`;
 
 export default function Products() {
   const { products } = useProducts();
 
   return (
-    <ul>
+    <List>
       {products.map(({
         id, name, price, deposit, thumbnailImage,
       }) => (
-        <li key={id}>
-          <h2>{name}</h2>
+        <ListItem key={id}>
+          <p>
+            <ThumbnailImage src={`${thumbnailImage}`} alt="상품 썸네일" />
+          </p>
+          <Title>{name}</Title>
           <p>
             일당:
             {' '}
@@ -20,12 +55,8 @@ export default function Products() {
             {' '}
             {deposit}
           </p>
-          <p>
-            썸네일:
-            <img src={`http://${thumbnailImage}`} alt="상품 썸네일" />
-          </p>
-        </li>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 export async function fetchProducts() {
-  const url = 'http://34.133.252.24:8080/rent/products/';
+  const url = 'http://3.35.70.80:8080/rent/products/';
+
   const { data } = await axios.get(url);
 
-  return data;
+  return data.content;
 }
 
 // TODO: delete below
