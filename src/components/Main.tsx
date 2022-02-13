3a322f055535dec8d5ad18a855d0b0c0f5e85bc3
gringrape <gringrape200@gmail.com>
2022-02-13 16:29:40 +0900

    경로 수정

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -16,7 +16,7 @@ const Layout = styled.div`
 export default function Main() {
   return (
     <Layout>
-      <Router basename="/villiar-front">
+      <Router basename="/">
         <Routes>
           <Route path="/" element={<ProductListPage />} />
           <Route path="/:id/reserve" element={<ReservePage />} />
